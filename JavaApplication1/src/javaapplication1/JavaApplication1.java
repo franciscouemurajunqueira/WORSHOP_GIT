7460c5e950082f03d7587e309c3504304abbb71c
Francisco <franciscouemura@gmail.com>
2017-12-07 18:05:31 -0200

    relembrando o escopo

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
        System.out.print("Hello World!");
+       System.out.print("Hello World2!");
+       //comentario
     }
     
 }
